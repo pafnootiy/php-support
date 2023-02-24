6493a1a7ad88bf50e42e284ea0baef22cb028db7
Irina Kasatkina <kasatkina235@gmail.com>
2023-02-24 07:31:01 +0300

    Move models from userapp to support_bot

--- support_bot/models.py
+++ support_bot/models.py
@@ -3,8 +3,14 @@ from django.db import models
 class Chat(models.Model):
     """Чат в Telegram."""
 
-    chat_id = models.CharField('Telegram Id чата с пользователем', max_length=64, db_index=True, null=False, blank=False)
-    dialogue_state = models.CharField('Этап диалога пользователя с ботом', max_length=64, default='START')
+    chat_id = models.CharField('Telegram Id чата с пользователем',
+                               max_length=64,
+                               db_index=True,
+                               null=False,
+                               blank=False)
+    dialogue_state = models.CharField('Этап диалога пользователя с ботом',
+                                      max_length=64,
+                                      default='START')
     
     @classmethod
     def get_dialogue_state(cls, chat_id):
@@ -26,4 +32,17 @@ class Chat(models.Model):
  
         cls.objects.filter(chat_id=chat_id).update(dialogue_state=dialogue_state)
         return dialogue_state
+
+        
+class Order(models.Model):
+    title = models.CharField(max_length=150)
+    description = models.TextField(blank=True)
+    created_at = models.DateField(auto_now_add=True)
+    update_at = models.DateField(auto_now=True)
+    is_published = models.BooleanField(default=True)
+
+
+class Storage(models.Model):
+    message = models.TextField(blank=True)
+    is_published = models.BooleanField(default=True)
 
--- userapp/models.py
+++ userapp/models.py
@@ -5,25 +5,3 @@ from django.db import models
 class User(AbstractUser):
     pass
 
-
-class Chat(models.Model):
-    """Чат в Telegram."""
-    chat_id = models.CharField('Telegram Id чата с пользователем',
-                               max_length=64, db_index=True,
-                               null=False,
-                               blank=False
-                               )
-    dialogue_state = models.CharField('Этап диалога пользователя с ботом',
-                                      default='START',
-                                      max_length=96
-                                      )
-class Order(models.Model):
-    title = models.CharField(max_length=150)
-    description = models.TextField(blank=True)
-    created_at = models.DateField(auto_now_add=True)
-    update_at = models.DateField(auto_now=True)
-    is_published = models.BooleanField(default=True)
-
-class Storage(models.Model):
-    message = models.TextField(blank=True)
-    is_published = models.BooleanField(default=True)
