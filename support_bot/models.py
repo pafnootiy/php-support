8c042f6986a8bf23cde0a3bdbeb527a944ff703e
Irina Kasatkina <kasatkina235@gmail.com>
2023-02-24 07:14:53 +0300

    Add bot main menu

--- support_bot/models.py
+++ support_bot/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+class Chat(models.Model):
+    """Чат в Telegram."""
+
+    chat_id = models.CharField('Telegram Id чата с пользователем', max_length=64, db_index=True, null=False, blank=False)
+    dialogue_state = models.CharField('Этап диалога пользователя с ботом', max_length=64, default='START')
+    
+    @classmethod
+    def get_dialogue_state(cls, chat_id):
+        """Получает из БД состояние диалога для чата."""
+
+        count = cls.objects.filter(chat_id=chat_id).count()
+        if count != 1:
+            return None
+
+        chats = cls.objects.filter(chat_id=chat_id)
+        return chats[0].dialogue_stage
+        
+    @classmethod
+    def update_dialogue_state(cls, chat_id, dialogue_state):
+        """Изменяет в БД состояние диалога для чата."""
+
+        if cls.objects.filter(chat_id=chat_id).count() != 1:
+            return None
+ 
+        cls.objects.filter(chat_id=chat_id).update(dialogue_state=dialogue_state)
+        return dialogue_state
+
